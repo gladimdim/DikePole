753d2892ece515b2a4eb1d0ee9364ddc623a4f1f
Dmytro Gladkyi <gladimdim@users.noreply.github.com>
2019-02-05 12:39:53 +0200

    Parsing json of Story

--- android/locadeserta/lib/story.dart
+++ android/locadeserta/lib/story.dart
@@ -3,6 +3,10 @@ class NextStory {
   final String name;
 
   NextStory({this.pid, this.name});
+
+  factory NextStory.fromJson(Map<String, dynamic> inputNext) {
+    return NextStory(pid: inputNext["pid"], name: inputNext["name"]);
+  }
 }
 
 class StoryNode {
@@ -10,12 +14,27 @@ class StoryNode {
   final String pid;
   final String background;
   final List<NextStory> links;
+
   StoryNode({this.text, this.pid, this.background, this.links});
+
+  factory StoryNode.fromJson(Map<String, dynamic> map) {
+    final inputLinks = map["links"] as List;
+
+    final links = new List<NextStory>();
+    if (inputLinks != null) {
+      links.addAll(inputLinks.map((i) => NextStory.fromJson(i)).toList());
+    }
+    return StoryNode(text: map["text"],
+        pid: map["pid"],
+        background: map["backgorund"],
+        links: links);
+  }
 }
 
 class Story {
   final String current;
   final List<StoryNode> passages;
+
   Story({this.passages, this.current = "1"});
 
   StoryNode getCurrentStory() {
@@ -30,7 +49,20 @@ class Story {
     }).elementAt(0);
   }
 
-  Map<String, dynamic> toJson() => {
-    "passages": passages
-  };
+  factory Story.fromJson(Map<String, dynamic> map) {
+    List<StoryNode> nodes = new List();
+    var input = map["passages"];
+    for (var node in input) {
+      nodes.add(StoryNode.fromJson(node));
+    }
+    return Story(
+        current: "1",
+        passages: nodes
+    );
+  }
+
+  Map<String, dynamic> toJson() =>
+      {
+        "passages": passages
+      };
 }
