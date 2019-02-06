2aec2163a010a83969f7a127cc0d4db654913661
Dmytro Gladkyi <gladimdim@gmail.com>
2019-02-06 22:51:49 +0200

    Added random generator for background images.

--- android/locadeserta/lib/passage.dart
+++ android/locadeserta/lib/passage.dart
@@ -1,9 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:locadeserta/story.dart';
 
 class Passage extends StatelessWidget {
   final StoryNode story;
-
+  final random  = new Random();
   Passage({this.story});
 
   @override
@@ -11,7 +13,7 @@ class Passage extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Image.asset("images/background/boat_2.jpg"),
+        Image.asset("images/background/boat_" + random.nextInt(2).toString() + ".jpg"),
         Expanded(
           flex: 1,
           child: SingleChildScrollView(
