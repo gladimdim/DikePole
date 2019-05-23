2e534d7e4983b544a673efbf66b6883d940626ee
Dmytro Gladkyi <gladimdim@gmail.com>
2019-05-23 17:32:07 +0300

    Release 1.50: showing user progress of story load.
    Completely remade firestore structure.

--- android/locadeserta/lib/models/Localizations.dart
+++ android/locadeserta/lib/models/Localizations.dart
@@ -3,7 +3,7 @@ import 'dart:core';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart' show SynchronousFuture;
 
-var version = "1.49";
+var version = "1.50";
 
 class LDLocalizations {
   LDLocalizations(this.locale);
