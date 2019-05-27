73d5ee941f4b3113175bd86f83d0d291468a148c
Dmytro Gladkyi <gladimdim@gmail.com>
2019-05-27 16:50:43 +0300

    Release 1.53.
    Added Cossack image as a loading screen alternative.

--- android/locadeserta/lib/models/Localizations.dart
+++ android/locadeserta/lib/models/Localizations.dart
@@ -3,7 +3,7 @@ import 'dart:core';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart' show SynchronousFuture;
 
-var version = "1.52";
+var version = "1.53";
 
 class LDLocalizations {
   LDLocalizations(this.locale);
