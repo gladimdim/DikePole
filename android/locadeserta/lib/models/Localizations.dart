c4d367d26426c921246f31b1b9c382ef53d85369
Dmytro Gladkyi <gladimdim@gmail.com>
2019-05-31 10:51:03 +0300

    Release 1.55.
    Removed the green rectangle which wrapped the whole app.
    
    Added animation for story transition.

--- android/locadeserta/lib/models/Localizations.dart
+++ android/locadeserta/lib/models/Localizations.dart
@@ -3,7 +3,7 @@ import 'dart:core';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart' show SynchronousFuture;
 
-var version = "1.54";
+var version = "1.55";
 
 class LDLocalizations {
   LDLocalizations(this.locale);
