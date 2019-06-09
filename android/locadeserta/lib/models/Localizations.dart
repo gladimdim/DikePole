ec0749c84e0dc1b0a80c3b230a6a7d3e9b17129f
Dmytro Gladkyi <gladimdim@gmail.com>
2019-06-09 23:11:00 +0300

    Release 1.59: Text instead of icons.
    Fixed spacing in loading view.

--- android/locadeserta/lib/models/Localizations.dart
+++ android/locadeserta/lib/models/Localizations.dart
@@ -3,7 +3,7 @@ import 'dart:core';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart' show SynchronousFuture;
 
-var version = "1.58";
+var version = "1.59";
 
 class LDLocalizations {
   LDLocalizations(this.locale);
