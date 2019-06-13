9862f29058742a2713f3121bda3dea4b5e821067
Dmytro Gladkyi <gladimdim@gmail.com>
2019-06-13 23:15:37 +0300

    Release 1.65. Corrected version.

--- android/locadeserta/lib/models/Localizations.dart
+++ android/locadeserta/lib/models/Localizations.dart
@@ -3,7 +3,7 @@ import 'dart:core';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart' show SynchronousFuture;
 
-var version = "1.64";
+var version = "1.65";
 
 class LDLocalizations {
   LDLocalizations(this.locale);
