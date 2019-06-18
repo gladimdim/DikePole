9ad764600e8e2e80a03c2d78f4f044564610ecbb
Dmytro Gladkyi <gladimdim@gmail.com>
2019-06-18 20:40:53 +0300

    Release 1.67: New Story detail view. Added new landing images.

--- android/locadeserta/lib/models/Localizations.dart
+++ android/locadeserta/lib/models/Localizations.dart
@@ -3,7 +3,7 @@ import 'dart:core';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart' show SynchronousFuture;
 
-var version = "1.66";
+var version = "1.67";
 
 class LDLocalizations {
   LDLocalizations(this.locale);
