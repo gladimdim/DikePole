dfa2be1e2b267831ed50208b6010bc19b695dd70
Dmytro Gladkyi <gladimdim@gmail.com>
2019-08-30 11:12:37 +0300

    Exported visible part of widget to png file

--- locadeserta/lib/story_view.dart
+++ locadeserta/lib/story_view.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:locadeserta/components/app_bar_custom.dart';
@@ -9,6 +15,7 @@ import 'package:locadeserta/models/catalogs.dart';
 import 'package:locadeserta/passage_view.dart';
 import 'package:locadeserta/models/persistence.dart';
 import 'package:locadeserta/models/story_bridge.dart';
+import 'package:path_provider/path_provider.dart';
 
 class StoryView extends StatefulWidget {
   final CatalogStory catalogStory;
@@ -23,6 +30,7 @@ class StoryView extends StatefulWidget {
 class _StoryViewState extends State<StoryView> {
   StoryBridge storyBridge;
   List _previousPassages = List();
+  GlobalKey _globalKey = new GlobalKey();
 
   @override
   void initState() {
@@ -72,24 +80,27 @@ class _StoryViewState extends State<StoryView> {
               children: <Widget>[
                 Padding(
                   padding: const EdgeInsets.only(top: 32),
-                  child: snapshot.hasData
-                      ? Passage(
-                          currentStory: currentStory,
-                          onNextOptionSelected: (s, i) async {
-                            _previousPassages.add(s);
-                            switch (s.type) {
-                              case PassageTypes.TEXT:
-                                await storyBridge.doContinue();
-                                break;
-                              case PassageTypes.IMAGE:
-                                await storyBridge.chooseChoiceIndex(i, s);
-                                break;
-                            }
-                            await Persistence.instance
-                                .saveStateToStorageForUser(widget.user,
-                                widget.catalogStory, storyBridge);
-                          })
-                      : Center(child: CircularProgressIndicator()),
+                  child: RepaintBoundary(
+                    key: _globalKey,
+                    child: snapshot.hasData
+                        ? Passage(
+                            currentStory: currentStory,
+                            onNextOptionSelected: (s, i) async {
+                              _previousPassages.add(s);
+                              switch (s.type) {
+                                case PassageTypes.TEXT:
+                                  await storyBridge.doContinue();
+                                  break;
+                                case PassageTypes.IMAGE:
+                                  await storyBridge.chooseChoiceIndex(i, s);
+                                  break;
+                              }
+                              await Persistence.instance
+                                  .saveStateToStorageForUser(widget.user,
+                                      widget.catalogStory, storyBridge);
+                            })
+                        : Center(child: CircularProgressIndicator()),
+                  ),
                 ),
                 AppBarCustom(
                   title: widget.catalogStory.title,
@@ -101,6 +112,24 @@ class _StoryViewState extends State<StoryView> {
                     AppBarObject(
                       onTap: _resetStory,
                       text: LDLocalizations.of(context).reset,
+                    ),
+                    AppBarObject(
+                      onTap: () async {
+                        try {
+                          RenderRepaintBoundary boundary =
+                              _globalKey.currentContext.findRenderObject();
+                          ui.Image image =
+                              await boundary.toImage(pixelRatio: 3.0);
+                          ByteData byteData = await image.toByteData(
+                              format: ui.ImageByteFormat.png);
+                          var pngBytes = byteData.buffer.asUint8List();
+                          _writeScreenshot(pngBytes);
+                          print("DONE");
+                        } catch (e) {
+                          print(e);
+                        }
+                      },
+                      text: LDLocalizations.of(context).shareStory,
                     )
                   ],
                 ),
@@ -111,6 +140,27 @@ class _StoryViewState extends State<StoryView> {
       },
     );
   }
+
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/screen1.png');
+  }
+
+  Future _writeScreenshot(Uint8List base64) async {
+    final file = await _localFile;
+
+    // Write the file.
+    return await file.writeAsBytes(base64);
+  }
+
+
+
 
   _resetStory() async {
     await storyBridge.resetState();
