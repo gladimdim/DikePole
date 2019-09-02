317f2aac236e5522d7b1ee62bfd8627339ff514c
Dmytro Gladkyi <gladimdim@gmail.com>
2019-09-02 15:45:34 +0300

    Added more details for process indication.

--- locadeserta/lib/export_pdf_view.dart
+++ locadeserta/lib/export_pdf_view.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:locadeserta/models/Localizations.dart';
 import 'package:locadeserta/models/catalogs.dart';
 import 'package:locadeserta/models/pdf_creator.dart';
 import 'package:locadeserta/models/story_history.dart';
@@ -22,54 +23,94 @@ class ExportToPDF extends StatefulWidget {
 }
 
 class _ExportToPDFState extends State<ExportToPDF> {
+  bool savingToFile = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Export"),
+        title: Text(LDLocalizations.of(context).shareStory),
       ),
-      body: StreamBuilder<Tuple2<double, String>>(
-          stream: widget.creator.decodeProgress,
-          builder: (context, snapshot) {
-            print(snapshot.hasData);
-            if (snapshot.hasData) {
-              return Padding(
-                padding: EdgeInsets.only(top: 12.0),
-                child: Center(
-                  child: Column(
-                    children: <Widget>[
-                      Text(
-                        "Processing image",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 20.0),
-                      ),
-                      Image(
-                        image: AssetImage(snapshot.data.item2),
-                      ),
-                      LinearProgressIndicator(
-                        value: snapshot.data.item1,
-                      ),
-                    ],
+      body: Center(
+        child: savingToFile
+            ? Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.import_export,
+                    size: 150,
                   ),
-                ),
-              );
-            } else {
-              return Center(
-                  child: RaisedButton(
-                child: Text("Export"),
-                onPressed: () => _onExportPressed(context),
-              ));
-            }
-          }),
+                  Text(
+                    "Saving to file",
+                    style:
+                        TextStyle(fontSize: 48.0, fontWeight: FontWeight.bold),
+                  )
+                ],
+              )
+            : StreamBuilder<Tuple2<double, String>>(
+                stream: widget.creator.decodeProgress,
+                builder: (context, snapshot) {
+                  print(snapshot.hasData);
+                  if (snapshot.hasData) {
+                    return Padding(
+                      padding: EdgeInsets.only(top: 12.0),
+                      child: Center(
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                              "Processing image",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 48.0),
+                            ),
+                            Image(
+                              image: AssetImage(snapshot.data.item2),
+                            ),
+                            LinearProgressIndicator(
+                              value: snapshot.data.item1,
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  } else {
+                    return Center(
+                        child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                            LDLocalizations.of(context)
+                                .dialogLongProcessDescription,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20.0), textAlign: TextAlign.center,),
+                        SizedBox(
+                          height: 150,
+                        ),
+                        RaisedButton(
+                          child: Text(LDLocalizations.of(context).start,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20.0)),
+                          onPressed: () => _onExportPressed(context),
+                        ),
+                      ],
+                    ));
+                  }
+                }),
+      ),
     );
   }
 
   _onExportPressed(BuildContext context) async {
+    setState(() {
+      savingToFile = false;
+    });
     final creator = widget.creator;
     await creator.loadImages();
     final pdf = await creator.toPdfDocument(
         widget.catalogStory.title, widget.catalogStory.author);
     final file = await _localFile;
+    setState(() {
+      savingToFile = true;
+    });
     await file.writeAsBytes(pdf.save());
     ShareExtend.share(file.path, "pdf");
   }
