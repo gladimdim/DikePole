fa0975940e53d6b2fc5e7bda71f181bd9c4e09a4
Dmytro Gladkyi <gladimdim@gmail.com>
2019-10-06 21:03:42 +0300

    Added missed filed

---
+++ locadeserta/lib/import_gladstories_view.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:locadeserta/animations/slideable_button.dart';
+import 'package:locadeserta/components/bordered_container.dart';
+import 'package:locadeserta/creator/components/fat_button.dart';
+import 'package:locadeserta/creator/story/persistence.dart';
+import 'package:locadeserta/creator/story/story.dart';
+import 'package:locadeserta/models/Auth.dart';
+import 'package:locadeserta/models/Localizations.dart';
+
+class ImportGladStoryView extends StatefulWidget {
+  static final String routeName = "/import_glad_story";
+
+  @override
+  _ImportGladStoryViewState createState() => _ImportGladStoryViewState();
+}
+
+class _ImportGladStoryViewState extends State<ImportGladStoryView> {
+  var _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(LDLocalizations.of(context).exportGladStoryToJson),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text(LDLocalizations.of(context).pasteFullJSON),
+            BorderedContainer(
+              child: TextField(
+                controller: _controller,
+                maxLines: 15,
+              ),
+            ),
+            SlideableButton(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FatButton(
+                  text: LDLocalizations.of(context).labelImport,
+                ),
+              ),
+              onPress: () async {
+                var story = Story.fromJson(_controller.text);
+                var user = await Auth().currentUser();
+                await StoryPersistence.instance
+                    .writeStory(user, story);
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
