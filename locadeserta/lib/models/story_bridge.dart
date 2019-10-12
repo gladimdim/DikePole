4916ed6cccf93ce6c898a6886e08e7e54cf82449
Dmytro Gladkyi <gladimdim@gmail.com>
2019-10-12 10:55:58 +0300

    Fixed issue when text could not be added with image.

--- locadeserta/lib/models/story_bridge.dart
+++ locadeserta/lib/models/story_bridge.dart
@@ -113,7 +113,7 @@ class StoryBridge {
     BackgroundImage.nextRandomForType(type);
     var randomImage = BackgroundImage.getRandomImageForType(type);
 
-    return story.canContinue
+    return type != null
         ? HistoryItemText(text)
         : HistoryItemImage([
             randomImage.getImagePathColored(),
@@ -204,11 +204,20 @@ class StoryBridge {
   }
 
   void _addCurrentPassage() {
-    story.storyHistory.addItem(
-      _createPassage(
-        story.currentText,
-        createImageType(),
-      ),
-    );
+    story.storyHistory.addItem(HistoryItemText(story.currentText));
+    if (!story.canContinue) {
+      var type = createImageType();
+      BackgroundImage.nextRandomForType(type);
+      var randomImage = BackgroundImage.getRandomImageForType(type);
+      story.storyHistory.addItem(
+        HistoryItemImage(
+          [
+            randomImage.getImagePathColored(),
+            randomImage.getImagePath(),
+          ],
+          type,
+        ),
+      );
+    }
   }
 }
