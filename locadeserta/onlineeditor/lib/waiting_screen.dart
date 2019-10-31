0276e3128ad362a8c5a7441169f445e0343bac04
Dmytro Gladkyi <gladimdim@gmail.com>
2019-10-31 15:55:46 +0200

    Removed fade image on waiting_screen

--- locadeserta/onlineeditor/lib/waiting_screen.dart
+++ locadeserta/onlineeditor/lib/waiting_screen.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/widgets.dart';
 import 'package:onlineeditor/Localizations.dart';
-import 'package:onlineeditor/animations/fade_images.dart';
 
 class WaitingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var height = MediaQuery.of(context).size.height * 0.5;
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
@@ -15,20 +13,6 @@ class WaitingScreen extends StatelessWidget {
             child: Text(
               LDLocalizations.lookingForHeroes,
               style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          Center(
-            child: Hero(
-              tag: "CossackHero",
-              child: TweenImage(
-                last: AssetImage("images/background/cossack_0.jpg"),
-                first: AssetImage("images/background/c_cossack_0.jpg"),
-                duration: 2,
-                height: height,
-              ),
             ),
           ),
         ],
