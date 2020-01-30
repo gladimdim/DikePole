60b6783e5a8609e409cbfd0c8ffcb67ce89ef6e7
Dmytro Gladkyi <gladimdim@gmail.com>
2020-01-30 22:34:50 +0200

    Added windows build and project.

--- sloboda/lib/main.dart
+++ sloboda/lib/main.dart
@@ -1,6 +1,20 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart'
+    show debugDefaultTargetPlatformOverride, kIsWeb;
 import 'package:flutter/widgets.dart';
 import 'package:sloboda/main_class.dart';
 
+void _setTargetPlatformForDesktop() {
+  // No need to handle macOS, as it has now been added to TargetPlatform.
+  if (Platform.isLinux || Platform.isWindows) {
+    debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+  }
+}
+
 void main() async {
+  if (!kIsWeb) {
+    _setTargetPlatformForDesktop();
+  }
   runApp(SlobodaApp());
 }
