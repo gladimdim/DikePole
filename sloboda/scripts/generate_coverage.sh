7ec3ca7f331ec31227bfa97948c7fd253f97870c
Dmytro Gladkyi <d.gladkyi@sap.com>
2020-01-31 15:05:03 +0200

    Fixed scripts for code coverage generation to create full coverage report.

--- sloboda/scripts/generate_coverage.sh
+++ sloboda/scripts/generate_coverage.sh
@@ -1,3 +1,5 @@
-flutter packages pub run test ./test/citybuilding --coverage=./coverage
-format_coverage --packages=.packages --report-on lib -i ./coverage/test/citybuilding/*.* -o ./coverage/lcov.info -l
-genhtml -o coverage ./coverage/lcov.info
+flutter packages pub run test ./test/citybuilding/sloboda_test.dart --coverage=./coverage/sloboda
+flutter packages pub run test ./test/citybuilding/resource_building_test.dart --coverage=./coverage/resource_building
+flutter packages pub global run coverage:format_coverage --packages=.packages --report-on lib -i ./coverage/sloboda/test/citybuilding/sloboda_test.dart.vm.json -o ./coverage/sloboda/lcov.info -l
+flutter packages pub global run coverage:format_coverage --packages=.packages --report-on lib -i ./coverage/resource_building/test/citybuilding/resource_building_test.dart.vm.json -o ./coverage/resource_building/lcov.info -l
+genhtml -o coverage ./coverage/sloboda/lcov.info ./coverage/resource_building/lcov.info
