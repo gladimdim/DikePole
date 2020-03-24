62c74517d26f3d22ff0189adbac47076d3bb2744
Dmytro Gladkyi <gladimdim@gmail.com>
2020-03-24 11:35:53 +0200

    Fixed madness with iOS failed build and startup.

--- locadeserta/lib/models/Localizations.dart
+++ locadeserta/lib/models/Localizations.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:locadeserta/loaders/url_parser.dart';
 import 'package:locadeserta/models/app_preferences.dart';
 
-var version = "1.112";
+var version = "1.113";
 
 String getDefaultOrUrlLanguage() {
   var urlLang = UrlParser.getLanguage();
