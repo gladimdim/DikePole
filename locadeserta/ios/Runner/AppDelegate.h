01bfda6599c4f1ea4e9e00c7de6ca64a1a85c2c8
Dmytro Gladkyi <gladimdim@gmail.com>
2020-07-17 21:58:04 +0300

    Release 1.117.
    New story.
    
    iOS release.

--- locadeserta/ios/Runner/AppDelegate.h
+++ locadeserta/ios/Runner/AppDelegate.h
@@ -1,8 +1,6 @@
 #import <Flutter/Flutter.h>
 #import <UIKit/UIKit.h>
-#import "InkStory.h"
 
 @interface AppDelegate : FlutterAppDelegate
-@property (strong) InkStory *inkStory;
-@property (strong) NSString *storyString;
+
 @end
--- locadeserta/ios/Runner/AppDelegate.m
+++ locadeserta/ios/Runner/AppDelegate.m
@@ -1,60 +1,11 @@
 #include "AppDelegate.h"
 #include <Flutter/Flutter.h>
 #include "GeneratedPluginRegistrant.h"
-#include "InkStory.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
-    FlutterViewController* controller = (FlutterViewController *) self.window.rootViewController;
-    
-    FlutterMethodChannel *inkChannel = [FlutterMethodChannel methodChannelWithName:@"gladimdim.locadeserta/Ink" binaryMessenger:controller];
-    [inkChannel setMethodCallHandler:^(FlutterMethodCall * _Nonnull call, FlutterResult result) {
-        if ([@"Init" isEqualToString:call.method]) {
-            self.storyString = call.arguments;
-            self.inkStory = [[[InkStory alloc] init] initWithJsonString:call.arguments];
-            result(@"success");
-        } else if ([@"canContinue" isEqualToString:call.method]) {
-            bool canContinue = [self.inkStory canContinue];
-            result([NSNumber numberWithBool:canContinue]);
-        } else if ([@"Continue" isEqualToString:call.method]) {
-            NSString* continueString = [self.inkStory continueStory];
-            result(continueString);
-        } else if ([@"getCurrentText" isEqualToString:call.method]) {
-            NSString* currentText = [self.inkStory currentText];
-            result(currentText);
-        } else if ([@"getCurrentChoices" isEqualToString:call.method]) {
-            NSArray* currentChoices = [self.inkStory currentChoices];
-            NSMutableArray* choices = [[NSMutableArray alloc] init];
-            for (NSDictionary *dict in currentChoices) {
-                NSString* text = dict[@"text"];
-                [choices addObject:text];
-            }
-            result(choices);
-        } else if ([@"getCurrentTags" isEqualToString:call.method]) {
-            NSArray* currentTags = [self.inkStory currentTags];
-            result(currentTags);
-        } else if ([@"chooseChoiceIndex" isEqualToString:call.method]) {
-            [self.inkStory chooseChoiceIndex:[call.arguments integerValue]];
-            result([self.inkStory currentText]);
-        } else if ([@"restoreState" isEqualToString:call.method]) {
-            NSDictionary *dict = call.arguments;
-            NSString *str = [NSString stringWithFormat:@"%@", [dict valueForKey:@"text"]];
-            [self.inkStory loadJson:str];
-            result(@"success");
-        } else if ([@"saveState" isEqualToString:call.method]) {
-            NSString *state =  [self.inkStory toJson];
-            return result(state);
-        } else if ([@"resetState" isEqualToString:call.method]) {
-            self.inkStory = [[[InkStory alloc] init] initWithJsonString:self.storyString];
-            result(@"success");
-        } else {
-            result(FlutterMethodNotImplemented);
-        }
-        
-    }];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
