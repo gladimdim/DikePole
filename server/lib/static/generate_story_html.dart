2f6f31190292036b08fa0ca56faa24beb722ab97
Dmytro Gladkyi <gladimdim@gmail.com>
2020-11-27 21:48:31 +0200

    Rendering markdown as SSR without JS

--- server/lib/static/generate_story_html.dart
+++ server/lib/static/generate_story_html.dart
@@ -1,3 +1,4 @@
+import 'package:markdown/markdown.dart';
 import 'package:server/static/generate_head.dart';
 
 String generateStoryHtml(String markdown) {
@@ -7,12 +8,8 @@ String generateStoryHtml(String markdown) {
        ${generateHead()}
         <body>
           <div>Дике Поле: Козацька Доля. Більше історій тут: <a href="https://locadeserta.com/">Всесвіт Дикого Поля</a></div>
-          <div id="content"></div>
-          <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
-          <script>
-            document.getElementById('content').innerHTML =
-              marked(`$markdown`);
-          </script>
+          <div id="content">
+          ${markdownToHtml(markdown)}</div>
         </body>
         </html>
     """;
