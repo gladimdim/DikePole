75754157c41b04bd843adc447a49e300ef9dd23b
Dmytro Gladkyi <gladimdim@gmail.com>
2021-03-13 16:02:40 +0200

    Added image to slidable action

--- locadeserta/lib/views/story_edit_view.dart
+++ locadeserta/lib/views/story_edit_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:gladstoriesengine/gladstoriesengine.dart';
 import 'package:locadeserta/creator/components/node_editor.dart';
 import 'package:locadeserta/models/Localizations.dart';
+import 'package:locadeserta/models/background_image.dart';
 
 class StoryEditView extends StatefulWidget {
   final Story story;
@@ -27,10 +28,11 @@ class _StoryEditViewState extends State<StoryEditView> {
     final page = widget.story.currentPage;
     List<Widget> widgets = page.nodes.map<Widget>(
       (node) {
+        var imageType = node.imageType;
         return Column(
           children: [
             Slidable(
-              actionPane: SlidableBehindActionPane(),
+              actionPane: SlidableScrollActionPane(),
               child: NodeEditor(
                 node: node,
               ),
@@ -38,6 +40,12 @@ class _StoryEditViewState extends State<StoryEditView> {
                 IconSlideAction(
                   icon: Icons.delete,
                 ),
+              ],
+              actions: [
+                if (imageType != null)
+                  Image(
+                    image: BackgroundImage.getAssetImageForType(imageType),
+                  )
               ],
             ),
             Align(
@@ -61,9 +69,11 @@ class _StoryEditViewState extends State<StoryEditView> {
         IconButton(
           icon: Icon(Icons.add),
           onPressed: () {
-            setState(() {
-              page.addNodeWithText("Empty");
-            });
+            setState(
+              () {
+                page.addNodeWithText("Empty");
+              },
+            );
           },
         ),
       );
